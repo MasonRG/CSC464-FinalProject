ac58258d13197cbfc48a72d160895d243bc7cbc8
Mason <roark.gamedev@gmail.com>
2018-12-05 19:49:42 -0800

    mesh generation scripts (incomplete)

---
+++ Assets/Scripts/MeshGeneration/ChunkedTerrain.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshGeneration
+{
+	public class ChunkedTerrain : MonoBehaviour
+	{
+		private int chunkSize;
+
+		public int chunks;
+
+		private Transform container;
+		public Transform Container
+		{
+			get
+			{
+				if (container == null)
+				{
+					container = new GameObject("terrain_chunks").transform;
+				}
+				return container;
+			}
+		}
+
+		private List<TerrainChunk> terrainChunks = new List<TerrainChunk>();
+
+
+		private void Start()
+		{
+			chunkSize = MapGenerator.mapChunkSize - 1;
+			GenerateChunks(chunks);
+		}
+
+		public void GenerateChunks(int size)
+		{
+			ClearChunks();
+
+			for (int y = 0; y < size; y++)
+			{
+				for (int x = 0; x < size; x++)
+				{
+					Vector2 chunkCoord = new Vector2(x, y);
+					terrainChunks.Add(new TerrainChunk(chunkCoord, chunkSize, Container));
+				}
+			}
+		}
+
+		public void ClearChunks()
+		{
+			foreach(var tc in terrainChunks)
+			{
+				tc.Clear();
+			}
+			terrainChunks.Clear();
+		}
+
+		public class TerrainChunk
+		{
+			GameObject meshObject;
+			Vector2 position;
+
+			public TerrainChunk(Vector2 coord, int size, Transform parent)
+			{
+				position = coord * size;
+				Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+				meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+				meshObject.transform.parent = parent;
+				meshObject.transform.localPosition = positionV3;
+				meshObject.transform.localScale = Vector3.one * size / 10f;
+			}
+
+			public void Clear()
+			{
+				if (meshObject != null)
+					Destroy(meshObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MeshGeneration/MapGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshGeneration
+{
+	public class MapGenerator : MonoBehaviour
+	{
+		public enum DrawMode { NoiseMap, ColorMap, Mesh }
+		public DrawMode drawMode;
+
+		public const int mapChunkSize = 254;
+		public float noiseScale;
+
+		public int octaves;
+		[Range(0f, 1f)]
+		public float persistance;
+		public float lacunarity;
+
+		public int seed;
+		public Vector2 offset;
+
+		public float meshHeightMultiplier;
+		public AnimationCurve meshHeightCurve;
+
+		public bool autoUpdate;
+
+		public TerrainType[] regions;
+
+		public void DrawMapInEditor()
+		{
+			MapData mapData = GenerateMapData();
+
+			MapDisplay display = FindObjectOfType<MapDisplay>();
+			if (drawMode == DrawMode.NoiseMap)
+			{
+				display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
+			}
+			else if (drawMode == DrawMode.ColorMap)
+			{
+				display.DrawTexture(TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+			}
+			else if (drawMode == DrawMode.Mesh)
+			{
+				display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+			}
+		}
+
+		private MapData GenerateMapData()
+		{
+			float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+			Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+
+			for(int y = 0; y < mapChunkSize; y++)
+			{
+				for (int x = 0; x < mapChunkSize; x++)
+				{
+					float currentHeight = noiseMap[x, y];
+					for(int i = 0; i < regions.Length; i++)
+					{
+						if (currentHeight <= regions[i].height)
+						{
+							colorMap[y * mapChunkSize + x] = regions[i].color;
+							break;
+						}
+					}
+				}
+			}
+
+			return new MapData(noiseMap, colorMap);
+		}
+
+
+		private void OnValidate()
+		{
+			if (lacunarity < 1) lacunarity = 1;
+			if (octaves < 1) octaves = 1;
+		}
+	}
+
+
+	[System.Serializable]
+	public struct TerrainType
+	{
+		public string name;
+		public float height;
+		public Color color;
+	}
+
+	public struct MapData
+	{
+		public float[,] heightMap;
+		public Color[] colorMap;
+
+		public MapData(float[,] heightMap, Color[] colorMap)
+		{
+			this.heightMap = heightMap;
+			this.colorMap = colorMap;
+		}
+	}
+}
